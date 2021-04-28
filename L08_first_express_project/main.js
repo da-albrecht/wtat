838280eee9e48d3dfd842f3ccd31716ed306c720
da-albrecht <albrecht.david3@gmail.com>
2021-04-28 12:55:45 +0200

    request data objects

--- L08_first_express_project/main.js
+++ L08_first_express_project/main.js
@@ -1,10 +1,14 @@
 const port = 3000,
- express = require("express"),
- app = express();
+    express = require("express"),
+    app = express();
 app.get("/", (req, res) => {
- res.send("Hello, Universe!");
-})
-.listen(port, () => {
- console.log(`The Express.js server has started and is listening
+        res.send("Hello, Universe!");
+        console.log(req.params);
+        console.log(req.body);
+        console.log(req.url);
+        console.log(req.query);
+    })
+    .listen(port, () => {
+        console.log(`The Express.js server has started and is listening
 ➥ on port number: ${port}`);
-});
+    });
